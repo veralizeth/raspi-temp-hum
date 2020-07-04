8b1acad0d33ef70dc577765d74e38cf0f0a65b40
Vera <lizeth.veraro@outlook.com>
2020-07-03 22:11:13 -0700

    Send date MQTT testing

---
+++ mqtt_send_date.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import sys
+import ssl
+import time
+import datetime
+import logging, traceback
+import paho.mqtt.client as mqtt
+
+IoT_protocol_name = "x-amzn-mqtt-ca"
+aws_iot_endpoint = "a2dixiflmrhbet-ats.iot.us-east-2.amazonaws.com" # <random>.iot.<region>.amazonaws.com
+url = "https://{}".format(aws_iot_endpoint)
+
+ca = "./certs/aws-root-cert.pem"
+cert = "./certs/5e3cf63a5a-certificate.pem.crt"
+private = "./certs/5e3cf63a5a-private.pem.key"
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(log_format)
+logger.addHandler(handler)
+
+def ssl_alpn():
+    try:
+        #debug print opnessl version
+        logger.info("open ssl version:{}".format(ssl.OPENSSL_VERSION))
+        ssl_context = ssl.create_default_context()
+        ssl_context.set_alpn_protocols([IoT_protocol_name])
+        ssl_context.load_verify_locations(cafile=ca)
+        ssl_context.load_cert_chain(certfile=cert, keyfile=private)
+
+        return  ssl_context
+    except Exception as e:
+        print("exception ssl_alpn()")
+        raise e
+
+if __name__ == '__main__':
+    topic = "test/date"
+    try:
+        mqttc = mqtt.Client()
+        ssl_context= ssl_alpn()
+        mqttc.tls_set_context(context=ssl_context)
+        logger.info("start connect")
+        mqttc.connect(aws_iot_endpoint, port=443)
+        logger.info("connect success")
+        mqttc.loop_start()
+
+        while True:
+            now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+            logger.info("try to publish:{}".format(now))
+            mqttc.publish(topic, now)
+            time.sleep(1)
+
+    except Exception as e:
+        logger.error("exception main()")
+        logger.error("e obj:{}".format(vars(e)))
+        logger.error("message:{}".format(e.message))
+        traceback.print_exc(file=sys.stdout)
+
